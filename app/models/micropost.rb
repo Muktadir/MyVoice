1725c52c9f8b4a0f73741c3d2c0878474fb23705
Muktadir <muktadir00@gmail.com>
2015-01-05 00:00:28 +0600

    update post

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,15 +1,15 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content
+  attr_accessible :title, :content
   belongs_to :user
 
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 300 }
 
   default_scope order: 'microposts.created_at DESC'
 
   def self.from_users_followed_by(user)
-    followed_user_ids = "SELECT followed_id FROM relationships
-                         WHERE follower_id = :user_id"
+    followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
           user_id: user.id)
   end
--- db/migrate/20120308210452_create_microposts.rb
+++ db/migrate/20120308210452_create_microposts.rb
@@ -1,6 +1,7 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
+      t.string :title
       t.string :content
       t.integer :user_id
 
